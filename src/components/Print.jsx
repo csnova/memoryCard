e12c8bf7be0f6a8a9a8c7ca3a1422fb1b1525cab
Chandler Nova <chandlersnova@gmail.com>
2023-11-09 15:27:51 -0800

    Added a Shuffle function for the card deck

--- src/components/Print.jsx
+++ src/components/Print.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import GameBoard from "./GameBoard";
 import Header from "./Header";
 import images from "./Images";
@@ -6,14 +6,33 @@ import "../styles/Print.css";
 
 const Print = () => {
   const [imagesDeck, setImagesDeck] = useState(images);
+  const [shuffledDeck, setShuffledDeck] = useState(images);
   const [score, setScore] = useState(0);
   const [highScore, setHighScore] = useState(0);
   const [level, setLevel] = useState(1);
 
+  //Shuffles the deck on load and when level changes
+  useEffect(() => {
+    let randomDeck = [...imagesDeck];
+    let currentIndex = shuffledDeck.length;
+    let randomIndex;
+    while (currentIndex > 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+      [randomDeck[currentIndex], randomDeck[randomIndex]] = [
+        randomDeck[randomIndex],
+        randomDeck[currentIndex],
+      ];
+    }
+    setShuffledDeck(randomDeck);
+  }, [level]);
+
+  console.log(shuffledDeck);
+
   return (
     <div className="fullPage">
       <Header score={score} highScore={highScore} level={level} />
-      <GameBoard imagesDeck={imagesDeck} />
+      <GameBoard imagesDeck={shuffledDeck} />
     </div>
   );
 };
